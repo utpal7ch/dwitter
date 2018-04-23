0c67428355b6f93f5fda6042d94007c35a60ae26
halumlom <halumlom@gmail.com>
2018-04-23 18:51:26 +0530

    changed port

--- src/server-config.ts
+++ src/server-config.ts
@@ -1,4 +1,4 @@
 export class ServerConfig {
-    public static readonly port: number = process.env.EXPRESS_PORT || 3000;
+    public static readonly port: number = process.env.EXPRESS_PORT || 5000;
     public static readonly isHttps: boolean = false;
 }
